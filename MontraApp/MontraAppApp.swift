aa57a20616bdc8e4e93030715b354683f0acd876
adityahimaone <adityahimaone@gmail.com>
2024-01-30 21:26:49 +0700

    Initial Commit

---
+++ MontraApp/MontraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraAppApp.swift
+//  MontraApp
+//
+//  Created by Aditya Himawan on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
